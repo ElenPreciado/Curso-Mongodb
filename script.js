e1380d0f6c36572b56ace2c883afbe66ec134f57
ElenPreciado <144728964+ElenPreciado@users.noreply.github.com>
2023-10-04 19:04:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// definir array
+
+const countries = ["Colombia", "España", "Canada", "Italia"]
+
+// primera función: map()
+// devuelve todos los elementos en el array.
+
+document.write((countries.map((myFunction)=>myFunction ) + "<br> "))
+
+
+//segunda función: every()
+// verifica si los elementos del array tienen cinco o más caracteres.
+
+document.write(countries.every((country)=>country.length >=5) + "<br> " )
+
+//tercera función: includes()
+// verifica si el elemento "Perú" está en el array
+
+document.write(countries.includes((country) => (country ==="Perú") ) + "<br>")
 
