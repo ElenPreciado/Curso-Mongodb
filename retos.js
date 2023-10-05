0337295bfdb889a7951a18be83ee63b7f2f546ef
ElenPreciado <144728964+ElenPreciado@users.noreply.github.com>
2023-10-04 21:35:30 -0500

    Create retos.js

---
+++ retos.js
@@ -0,0 +1,82 @@
+const getNestedRecordsExample = () => [
+    {
+      id: 1,
+      nombre: 'Camila Lopez',
+      detalles: {
+        descripcion: 'matricula normal',
+        contacto: {
+          email: 'clopez@mail.com',
+		  telefono: 83242834
+        },
+		valorMatricula: 200000
+      },
+    },
+    {
+      id: 2,
+      nombre: 'Pedro Perez',
+      detalles: {
+        descripcion: 'beca',
+        contacto: {
+          email: 'pperez@mail.com',
+		  telefono: 83242834
+        },
+		valorMatricula: 0
+      },
+    },
+    {
+      id: 3,
+      nombre: 'Juan Yarce',
+      detalles: {
+        descripcion: 'matricula normal',
+        contacto: {
+          value: 'jyarce@mail.com',
+		  telefono: 1122334455
+        },
+		valorMatricula: 400000
+      },
+    },
+	{
+      id: 4,
+      nombre: 'Alberto Benitez',
+      detalles: {
+        descripcion: 'matricula condicional',
+        contacto: {
+          value: 'abenitez@mail.com',
+		  telefono: 234534566
+        },
+		valorMatricula: 600000
+      },
+    },
+  ];
+
+
+
+const matriculaTotal = getNestedRecordsExample().reduce((accumulator, matricula) =>accumulator+matricula.detalles.valorMatricula, 0) 
+
+document.write("El total de la matrícula es: $" + matriculaTotal)
+
+
+// sobre la misma data o función en un array obtener los datos de contacto teléfono de los estudiantes
+
+const contactoTel = getNestedRecordsExample().filter(contacto=> 
+  {
+    document.write("<br> " + "Teléfono de contacto: "+ contacto.detalles.contacto.telefono, 0);
+   
+  })
+
+
+//obtener el nombre del estudiante con mayor valor de matricula
+  
+
+
+const mayorMatricula = getNestedRecordsExample().filter(matricula, estudiante)=> 
+  {
+    const nombre = estudiante.detalles.nombre, 0
+
+
+   
+  })
+
+
+
+//Obtener el correo del estudiante con matricula condicional
